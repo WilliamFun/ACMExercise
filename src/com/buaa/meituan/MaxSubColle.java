c749b22f589d977178ca5d926391e70b8b6d68b3
WilliamFun <1509325537@qq.com>
2022-03-12 14:55:50 +0800

    Input

---
+++ src/com/buaa/meituan/MaxSubColle.java
@@ -0,0 +1,12 @@
+package com.buaa.meituan;
+
+
+//给一个可重集合，选出一个子集，使得任意两个数不连续，求最大子集大小
+public class MaxSubColle {
+
+    int 
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/com/buaa/test/Test1.java
@@ -0,0 +1,8 @@
+package com.buaa.test;
+
+//一个可重集合，
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
