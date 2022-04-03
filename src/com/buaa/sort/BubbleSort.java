71e50132cb563dababf116f395d468985cdbf4b7
WilliamFun <42149220+WilliamFun@users.noreply.github.com>
2022-04-03 10:00:08 +0800

    冒泡排序

--- src/com/buaa/sort/BubbleSort.java
+++ src/com/buaa/sort/BubbleSort.java
@@ -2,29 +2,73 @@ package com.buaa.sort;
 //nlog(2n)
 //比较相邻的两个元素将大的放在后面
 public class BubbleSort {
-    public void bubble(int[] arr,int n){//将最大的排到最后
-        for(int i=0;i<n-1;i++){
-            if(arr[i]>arr[i+1]){
-                int temp=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=temp;
+    public void bubbleSort(int[] arr){
+        for (int i = 0;i<arr.length-1;i++){
+            for (int j = 0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]) swap(arr,j,j+1);
             }
         }
     }
-    public void bubbleSort(int[] arr,int n){
-        for(int i=n;i>=1;i--){
-            bubble(arr,i);
+
+    //冒泡优化1：如果一轮比较中没有发生过交换，则立即停止排序，因为此时剩余数字一定已经有序了
+    public void bubbleSort_2(int[] arr){
+        boolean swapped = true;
+        for (int i = 0;i<arr.length-1;i++){
+            if(!swapped) break;
+            swapped = false;
+            for (int j = 0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(arr,j,j+1);
+                    swapped = true;
+                }
+            }
         }
     }
 
+    //冒泡优化2：
+    public void bubbleSort_3(int[] arr){
+        boolean swapped = true;
+        int lastIndexOfUnsortedElement = arr.length-1;
+        int lastSwappedIndex = -1;
+        while (swapped){
+            swapped = false;
+            for (int i = 0;i<lastIndexOfUnsortedElement;i++){
+                if(arr[i]>arr[i+1]){
+                    swap(arr,i,i+1);
+                    swapped = true;
+                    lastSwappedIndex = i;
+                }
+            }
+            lastIndexOfUnsortedElement = lastSwappedIndex;
+        }
+    }
+
+
+
+
     public static void main(String[] args) {
+        //它的空间复杂度为 O(1)，时间复杂度为 O(n^2)，第二种、第三种冒泡排序由于经过优化，最好的情况下只需要O(n)O(n)的时间复杂度。
         BubbleSort bubbleSort=new BubbleSort();
         int[] arr={1,5,8,9,4,6,7,2,15,16,2};
-        bubbleSort.bubbleSort(arr,11);
+        bubbleSort.bubbleSort_3(arr);
         for(int i:arr){
-            System.out.println(i);
+            System.out.print(i+" ");
         }
 
+    }
+
+    public void swap(int[] arr,int i,int j){
+//        int temp = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = temp;
+
+//        arr[i] = arr[i]^arr[j];
+//        arr[j] = arr[i]^arr[j];
+//        arr[i] = arr[i]^arr[j];
+
+          arr[i] = arr[i]+arr[j];
+          arr[j] = arr[i]-arr[j];
+          arr[i] = arr[i]-arr[j];
     }
 
 }
--- src/com/buaa/sort/SelectionSort.java
+++ src/com/buaa/sort/SelectionSort.java
@@ -1,37 +1,18 @@
 package com.buaa.sort;
-//n平方
-//每次遍历没排好序是数组，将最大值放到最后面
-public class SelectionSort {
-    public int findMaxPos(int[] arr,int n){
-        int max=arr[0];
-        int pos=0;
-        for (int i=0;i<n;i++){
-            if(arr[i]>max){
-                max=arr[i];
-                pos=i;
-            }
-        }
-        return pos;
-    }
 
-    void select(int arr[],int n){
-        while (n>1){
-            int pos=findMaxPos(arr,n);
-            int temp=arr[pos];
-            arr[pos]=arr[n-1];
-            arr[n-1]=temp;
-            n--;
-        }
+public class SelectionSort {
+
+    //选择排序：双重循环遍历数组，每经过一轮比较，找到最小元素的下标，将其交换至首位
+    public void selectionSort(int[] arr){
+
     }
 
     public static void main(String[] args) {
-//        int[] arr={1,5,8,9,4,6,7,2,15,16,2};
-//        SelectionSort selectionSort=new SelectionSort();
-//        selectionSort.select(arr,11);
-//        for(int i:arr){
-//            System.out.println(i);
-//        }
-        String str="liupengyu";
-        System.out.println(str.substring(0,4));;
+        int[] arr={1,5,8,9,4,6,7,2,15,16,2};
+        SelectionSort selectionSort=new SelectionSort();
+        selectionSort.selectionSort(arr);
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
     }
 }
