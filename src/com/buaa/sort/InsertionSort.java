3e8f29a2e5436e027bf87562be2c30e55ff80a35
WilliamFun <42149220+WilliamFun@users.noreply.github.com>
2022-04-04 00:41:13 +0800

    n2复杂度排序

--- src/com/buaa/sort/InsertionSort.java
+++ src/com/buaa/sort/InsertionSort.java
@@ -1,38 +1,47 @@
 package com.buaa.sort;
 //将未排序的数字插入到排好序的数组中
 public class InsertionSort {
-    void insert(int[] arr,int n){
-        int key=arr[n];
-        int i=n;
-        while (arr[i-1]>key){
-            arr[i]=arr[i-1];
-            i--;
-            if(i==0) break;
-        }
-        arr[i]=key;
-    }
 
-    void insertSort(int[] arr,int n){
-//        for(int i=1;i<n;i++){
-//            insert(arr,i);
-//        }
-        for(int i=1;i<n;i++){
-            int key=arr[i];
-            int insertPos=i;
-            while(arr[insertPos-1]>key){
-                arr[insertPos]=arr[insertPos-1];
-                insertPos--;
-                if(insertPos==0) break;
+    //交换法：在新数字插入过程中，不断与前面的数字交换，直到找到自己合适的位置。
+    public void insertSort_swap(int[] arr){
+        for (int i = 1;i<arr.length;i++){
+            int j = i;
+            while (j>=1&&arr[j]<arr[j-1]){
+                swap(arr,j,j-1);
+                j--;
             }
         }
     }
 
+    //优化方案：让新插入的数字先进行比较，前面比它大的数字不断向后移动，直到找到适合这个新数字的位置后，新数字只做一次插入操作即可。
+    //移动法：在新数字插入过程中，与前面的数字不断比较，前面的数字不断向后挪出位置，当新数字找到自己的位置后，插入一次即可
+    public void insertSort_move(int[] arr){
+        for (int i = 1;i<arr.length;i++){
+            int currentNum = arr[i];
+            int j = i-1;
+            while (j>0&&currentNum<arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = currentNum;
+        }
+    }
+
+    public static void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    //插入排序过程需要两层循环，时间复杂度为 O(n^2)
+    //只需要常量级的临时变量，空间复杂度为 O(1)。
+
     public static void main(String[] args) {
         int[] arr={1,5,8,9,4,6,7,2,15,16,2};
         InsertionSort selectionSort=new InsertionSort();
-        selectionSort.insertSort(arr,11);
+        selectionSort.insertSort_move(arr);
         for(int i:arr){
-            System.out.println(i);
+            System.out.print(i+" ");
         }
     }
 }
--- src/com/buaa/sort/SelectionSort.java
+++ src/com/buaa/sort/SelectionSort.java
@@ -4,13 +4,66 @@ public class SelectionSort {
 
     //选择排序：双重循环遍历数组，每经过一轮比较，找到最小元素的下标，将其交换至首位
     public void selectionSort(int[] arr){
+        for (int i = 0;i<arr.length-1;i++){
+            int minindex = i;
+            for (int j = i+1;j<arr.length;j++){
+                if(arr[j]<arr[minindex]) minindex = j;
+            }
+            swap(arr,i,minindex);
+        }
+    }
+    /*
+    冒泡排序和选择排序有什么异同？
+    相同点：
+    都是两层循环，时间复杂度都为 O(n^2);
+    都只使用有限个变量，空间复杂度 O(1)O(1)。
+    不同点：
+    冒泡排序在比较过程中就不断交换；而选择排序增加了一个变量保存最小值 / 最大值的下标，遍历完成后才交换，减少了交换次数。
+    事实上，冒泡排序和选择排序还有一个非常重要的不同点，那就是：
+    冒泡排序法是稳定的，选择排序法是不稳定的。
 
+    而选择排序中，最小值和首位交换的过程可能会破坏稳定性。
+    比如数列：[2, 2, 1]，在选择排序中第一次进行交换时，原数列中的两个 2 的相对顺序就被改变了，因此，我们说选择排序是不稳定的。
+
+     */
+
+    //二元选择排序：每轮选择时记录最小值和最大值，可以把数组需要遍历的范围缩小一倍
+    public void selectionSort2(int[] arr){
+        for (int i = 0;i<arr.length/2;i++){
+            int minindex = i;
+            int maxindex = i;
+            for (int j = i+1;j<arr.length-i;j++){
+                if(arr[j]>arr[maxindex]) maxindex = j;
+                if(arr[j]<arr[minindex]) minindex = j;
+            }
+            if(minindex==maxindex) break;
+            swap(arr,minindex,i);
+            if(maxindex==i) maxindex = minindex;
+            swap(arr,maxindex,arr.length-1-i);
+        }
+    }
+
+
+
+    public void swap(int[] arr,int i,int j){
+        if(i==j) return;
+//        int temp = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = temp;
+
+//        arr[i] = arr[i]^arr[j];
+//        arr[j] = arr[i]^arr[j];
+//        arr[i] = arr[i]^arr[j];
+
+        arr[i] = arr[i]+arr[j];
+        arr[j] = arr[i]-arr[j];
+        arr[i] = arr[i]-arr[j];
     }
 
     public static void main(String[] args) {
         int[] arr={1,5,8,9,4,6,7,2,15,16,2};
         SelectionSort selectionSort=new SelectionSort();
-        selectionSort.selectionSort(arr);
+        selectionSort.selectionSort2(arr);
         for(int i:arr){
             System.out.print(i+" ");
         }
