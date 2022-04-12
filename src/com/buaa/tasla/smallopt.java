3fba6d163d111f7f9e54b85ff717ed6c20e17f59
WilliamFun <1509325537@qq.com>
2022-04-12 20:03:20 +0800

    tesla

---
+++ src/com/buaa/tasla/smallopt.java
@@ -0,0 +1,22 @@
+package com.buaa.tasla;
+
+import java.util.Arrays;
+
+public class smallopt {
+
+    public static void main(String[] args) {
+        int[] A = new int[]{1, 3, 6, 4, 1, 2};
+
+        Arrays.sort(A);
+        int cur = 1;
+        for(int i = 0;i<A.length;i++){
+            if(A[i]==cur){
+                cur++;
+            }
+            if(A[i]>cur){
+                break;
+            }
+        }
+        System.out.println(cur);
+    }
+}
